2fe54182b739eff5c14af5123ba78f29ffaca70e
exashox <exashox@gmail.com>
2017-05-22 09:19:39 +0200

    Up

---
+++ src/java/Model/Kontaktformular.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author ExaShox
+ */
+public class Kontaktformular {
+
+    private String firstname, lastname, date, description, message;
+
+    public Kontaktformular(String firstname, String lastname, String date, String description, String message) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.date = date;
+        this.description = description;
+        this.message = message;
+    }
+
+    public void showMeWhatYouGot() {
+        System.out.println("Vorname: " + firstname);
+        System.out.println("Nachname: " + lastname);
+        System.out.println("Datum: " + date);
+        System.out.println("Beschreibung: " + description);
+        System.out.println("Nachricht: " + message);
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
